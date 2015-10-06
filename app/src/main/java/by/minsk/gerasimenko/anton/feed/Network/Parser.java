0fb07dc915e4215b7d8b64870c014e78c4cbfb22
TrubkaU <dubrava123@gmail.com>
2015-10-06 16:35:48 +0300

    Start

--- app/src/main/java/by/minsk/gerasimenko/anton/feed/Network/Parser.java
+++ app/src/main/java/by/minsk/gerasimenko/anton/feed/Network/Parser.java
@@ -1,15 +1,8 @@
 package by.minsk.gerasimenko.anton.feed.Network;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import java.io.IOException;
+
 import java.io.InputStream;
 
-import by.minsk.gerasimenko.anton.feed.models.News;
-import by.minsk.gerasimenko.anton.feed.models.Simple;
-import edu.usf.cutr.javax.xml.stream.XMLInputFactory;
 
 /**
  * Created by gerasimenko on 02.10.2015.
@@ -19,7 +12,6 @@ public class Parser {
     void p(InputStream stream){
 
 
-        XMLInputFactory f = XMLInputFactory.newInstance();
 
 
     }
